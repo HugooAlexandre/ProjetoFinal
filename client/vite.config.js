8e9ec5cb33427d024783372ba137767ac1968ba4
HugooAlexandre <168734655+HugooAlexandre@users.noreply.github.com>
2026-01-31 18:24:17 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5173, // Porta padrão do Vite
+    port: 5173, // Porta do Vite
   }
+
 })
